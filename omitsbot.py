298182d218bdda9da9c5cf5fbce3e66e50dbc5b9
DanStimo <stimodj@outlook.com>
2025-04-20 00:30:08 +0100

    Update omitsbot.py

--- omitsbot.py
+++ omitsbot.py
@@ -326,7 +326,9 @@ class ClubDropdown(discord.ui.Select):
 
         # ✅ Use PrintRecordButton with timeout
         view = PrintRecordButton(stats, selected['clubInfo']['name'].upper())
-        view.message = await interaction.response.edit_message(embed=embed, view=view)
+        await interaction.edit_original_response(embed=embed, view=view)
+
+        return
     
         await interaction.message.edit(
             embed=embed,
